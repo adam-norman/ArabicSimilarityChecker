0d119bc72211d75d7d533412cdddfd7093495100
adam-norman <ma7moud.asp@gmail.com>
2020-06-16 15:45:10 +0200

    export to csv

--- LangHelper.py
+++ LangHelper.py
@@ -4,16 +4,17 @@ from Cosine import *
 from nltk.stem.isri import ISRIStemmer
 import os
 import math
-
+import csv
 
 directory = r'D:\Work\NLP\word files'
-listOfFiles=[]
+listOfFiles = []
 for filename in os.listdir(directory):
     if filename.endswith(".doc") or filename.endswith(".docx"):
         listOfFiles.append(os.path.join(directory, filename))
     else:
         continue
 
+NumberOfFiles = len(listOfFiles)
 DocumentsStore = []
 print("reading files Started")
 for file in listOfFiles:
@@ -27,16 +28,15 @@ index = 0
 AllRoots = []
 print("Steming files Started")
 for fileContent in DocumentsStore:
-    fileStems=[]
+    fileStems = []
     for word in word_tokenize(fileContent):
         fileStems.append((st.stem(word)))
     FilesStemsDic[listOfFiles[index]] = fileStems
     index = index + 1
 
-
 print("Steming files Ended")
 index = 0
-DistinctStemsList=[]
+DistinctStemsList = []
 print("building Distinct Stems List started")
 for key in FilesStemsDic:
     for stem in FilesStemsDic[key]:
@@ -51,24 +51,32 @@ for stem in DistinctStemsList:
     for key in FilesStemsDic:
         for fileStem in FilesStemsDic[key]:
             if stem == fileStem:
-                StemFileMatrix[r][f] = StemFileMatrix[r][f]+1
+                StemFileMatrix[r][f] = StemFileMatrix[r][f] + 1
         f = f + 1
     r = r + 1
 
-r=0
+r = 0
 for i in range(len(StemFileMatrix)):
     numbersPow2 = []
     for n in StemFileMatrix[i]:
         numbersPow2.append(n ** 2)
 
     itemsSum = sum(numbersPow2)
-    f=0
+    f = 0
     for n in StemFileMatrix[i]:
-        StemFileMatrix[r][f] = n/math.sqrt(itemsSum)
+        StemFileMatrix[r][f] = n / math.sqrt(itemsSum)
         f = f + 1
     r = r + 1
 
-col1 = [val[1] for val in StemFileMatrix]
-col2 = [val[2] for val in StemFileMatrix]
-print("similarity between file & file2:")
-print( GetCos(col1,col2))
+csvFileData = [["File Name", "File Name", "Similarity"]]
+for i in range(NumberOfFiles):
+    for j in range(NumberOfFiles):
+        col1 = [val[i] for val in StemFileMatrix]
+        col2 = [val[j] for val in StemFileMatrix]
+        similarityVal = GetCos(col1, col2)
+        csvFileData.append([listOfFiles[i], listOfFiles[j], similarityVal])
+
+myFile = open('similarity.csv', 'w')
+with myFile:
+    writer = csv.writer(myFile)
+    writer.writerows(csvFileData)
